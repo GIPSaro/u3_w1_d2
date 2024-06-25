253c1f569cea6ad9b1db89fdf5f3a557aabfa581
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2024-06-25 17:01:00 +0200

    commit2

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,17 +1,29 @@
-import { Card } from "react-bootstrap";
+import { Card, Container, Row, Col } from "react-bootstrap";
+import books from "../books/fantasy.json";
 
-const AllTheBooks = ({ sectionTitle, data }) => {
+const AllTheBooks = () => {
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    <Container>
+      <Row>
+        {books.map((books) => {
+          return (
+            <Col>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+              ;
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
   );
 };
 export default AllTheBooks;
