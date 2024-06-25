9bd55d753ee6913838ae6dc01218846d0fa56824
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2024-06-25 17:21:51 +0200

    commit3

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,29 +1,30 @@
+import { Component } from "react";
+import Badge from "react-bootstrap/Badge";
 import { Card, Container, Row, Col } from "react-bootstrap";
-import books from "../books/fantasy.json";
+import Books from "../books/scifi.json";
 
-const AllTheBooks = () => {
-  return (
-    <Container>
-      <Row>
-        {books.map((books) => {
-          return (
-            <Col>
-              <Card style={{ width: "18rem" }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-              ;
-            </Col>
-          );
-        })}
-      </Row>
-    </Container>
-  );
-};
+class AllTheBooks extends Component {
+  state = {};
+  render() {
+    return (
+      <Container className="fluid">
+        <Row>
+          {Books.map((book) => {
+            return (
+              <Col xs={12} md={3} key={book.asin}>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src={book.img} />
+                  <Card.Body>
+                    <Card.Title>{book.title}</Card.Title>
+                    <Badge bg="info">{book.price}$</Badge>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    );
+  }
+}
 export default AllTheBooks;
